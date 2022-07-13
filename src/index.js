7792821ea577391a28b03ca55062a4164cacdc2a
MARS <Sergei-6@bk.ru>
2022-07-13 11:55:14 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+  if (matrix) {
+    matrix.reduce((gradeSumm, value, index) => {
+      (index === 0 || index % 2 === 0) ? value : value.reverse();
+      return gradeSumm + value;
+    }, []);
+    return matrix.flat();
+  } else {
+    return [];
+  }
+};
